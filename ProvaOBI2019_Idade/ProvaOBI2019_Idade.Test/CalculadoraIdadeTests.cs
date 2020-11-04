b9b876e1deab748960a3df183ff2b6d977d8eac5
Joabe Alexandre <joabe_alexandre@hotmail.com>
2020-11-03 22:39:35 -0300

    .gitignore fix

---
+++ ProvaOBI2019_Idade/ProvaOBI2019_Idade.Test/CalculadoraIdadeTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ProvaOBI2019_Idade.Test
+{
+    public class CalculadoraIdadeTests
+    {
+        private readonly CalculadoraIdade calculadora;
+
+        public CalculadoraIdadeTests()
+        {
+            calculadora = new CalculadoraIdade();
+        }
+
+        [Theory]
+        [InlineData(52, 14, 18, 20)]
+        [InlineData(47, 21, 9, 21)]
+        public void CalcularIdadeFilhoMaisVelho_DeveRetornarAIdadeFilhoMaisVelho(int idadeMae, int idadeFilhoA, int idadeFilhoB, int expected)
+        {
+            //Act
+            var result = calculadora.CalcularIdadeFilhoMaisVelho(idadeMae, idadeFilhoA, idadeFilhoB);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(10, 14, 18, 0)]
+        [InlineData(150, 21, 9, 0)]
+        public void CalcularIdadeFilhoMaisVelho_DeveTratarIdadeMaeInvalida(int idadeMae, int idadeFilhoA, int idadeFilhoB, int expected)
+        {
+            //Act
+            var result = calculadora.CalcularIdadeFilhoMaisVelho(idadeMae, idadeFilhoA, idadeFilhoB);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(50, 14, 80, 0)]
+        [InlineData(60, 75, 9, 0)]
+        [InlineData(47, 9, 9, 0)]
+        public void CalcularIdadeFilhoMaisVelho_DeveTratarIdadeFilhoInvalida(int idadeMae, int idadeFilhoA, int idadeFilhoB, int expected)
+        {
+            //Act
+            var result = calculadora.CalcularIdadeFilhoMaisVelho(idadeMae, idadeFilhoA, idadeFilhoB);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ ProvaOBI2019_Idade/Readme.md
@@ -0,0 +1,15 @@
+# Orientações
+
+* Utilizar a estrutura disponibilizada
+* O método CalcularIdadeFilhoMaisVelho() realizará o cálculo necessário para o desafio
+* Retornar o valor 0 se os parâmetros forem inválidos
+
+## Testando o projeto
+
+Abrir o prompt de comando na raiz da solution e executar o comando "dotnet test".
+
+O resultado deve ser: 
+
+* Total tests: 7
+* Passed: 7
+* Failed: 0
