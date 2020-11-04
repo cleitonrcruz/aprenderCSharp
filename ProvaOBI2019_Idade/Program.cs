cd8dbfae3d1d1db446c9d8f01ae3114db7d553e2
Cleiton Cruz <cleiton.rcruz@gmail.com>
2020-11-04 08:58:37 -0300

    PDF

---
+++ ProvaOBI2019_Idade/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ProvaOBI2019_Idade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Digite o primeiro número: ");
+                var soma = Convert.ToDecimal(Console.ReadLine());
+                Console.Write("Digite o numero a ser dividido: ");
+                var quantidade = Convert.ToDecimal(Console.ReadLine());
+                var media = soma / quantidade;
+                Console.WriteLine("Média calculada: " + media);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("O valor digitado não é um número válido.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("O valor digitado excede os limites suportados.");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Não é possível dividir por zero.");
+            }
+        }
+    }
+}
